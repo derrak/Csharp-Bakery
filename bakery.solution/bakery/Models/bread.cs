2b148abb8773a622b8871d89a0f4003243149c69
Derrak Richard <derrak@gmail.com>
2022-07-15 10:21:20 -0700

    passed first test

--- bakery.solution/bakery/Models/bread.cs
+++ bakery.solution/bakery/Models/bread.cs
@@ -1,1 +1,29 @@
-//Buy 2, get 1 free (every 3rd loaf of bread is free.). A single loaf costs $5, two loaves costs $10, and three loaves cost $10.
+namespace Bakery
+{
+  public class Bread
+  {
+   int loafPrice = 5;
+
+    public bool BreadPricing(int breadCount)
+    {
+    // int breadPrice = 0;
+    // for(int i = 1; i <= breadCount; i++){
+      if (breadCount % 3 == 0)
+      {
+        return true;
+      }
+      else{return false;}
+    }
+    }
+  }
+
+
+/* Buy 2, get 1 free (every 3rd loaf of bread is free.). 
+A single loaf costs $5, two loaves costs $10, 
+and three loaves cost $10. 
+
+
+if i%3 == 0 {breadCost+0}
+else breadCost + 5;
+
+*/
