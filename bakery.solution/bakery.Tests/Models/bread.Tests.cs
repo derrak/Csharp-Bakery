3525ef67d54612bcc607b439cd530beca4ca3598
Derrak Richard <derrak@gmail.com>
2022-07-16 15:05:27 -0700

    second test passed; single bread price passed into bread object

--- bakery.solution/bakery.Tests/Models/bread.Tests.cs
+++ bakery.solution/bakery.Tests/Models/bread.Tests.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bakery;
 
-namespace Bread1.Tests
+namespace Bakery.Tests
 {
   [TestClass]
   public class BreadTests
@@ -11,10 +11,24 @@ namespace Bread1.Tests
     {
       //Arrange
       //Act
-      Bread newBreadObj = new Bread();
+      Bread newBreadObj = new Bread(1,5);
 
       //Assert
       Assert.AreEqual(typeof(Bread), newBreadObj.GetType());
+    }
+
+        [TestMethod]
+    public void Bread_SingleLoafCost_BreadPrice()
+    {
+      //Arrange
+      int price = 5;
+      Bread newBreadObj = new Bread(1,5);
+
+      //Act
+      int singleLoafPrice = newBreadObj.BreadPrice;
+
+      //Assert
+      Assert.AreEqual(price, singleLoafPrice);
     }
   }
 }
